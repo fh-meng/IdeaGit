442318d016530daee89cd6cb3923393a41c58f4d
mxh_glb <GoodMorning_glb@163.com>
2020-06-30 14:48:39 +0800

    create branch commit

--- IdeaGit/src/main/java/com/iotek/git/HelloGit.java
+++ IdeaGit/src/main/java/com/iotek/git/HelloGit.java
@@ -9,6 +9,7 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello git...");
         System.out.println("update1...");
+        System.out.println("创建分支...");
     }
 
 }
