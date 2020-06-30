2bbaa16af6484992ed2450e46a483b3fa03e497e
mxh_glb <GoodMorning_glb@163.com>
2020-06-30 14:39:01 +0800

    My first commit

---
+++ IdeaGit/src/main/java/com/iotek/git/HelloGit.java
@@ -0,0 +1,13 @@
+package com.iotek.git;
+
+/**
+ * @ author mxh
+ * @ create 2020-06-30 14:26
+ */
+public class HelloGit {
+
+    public static void main(String[] args) {
+        System.out.println("hello git...");
+    }
+
+}
